5d9648943c1e9e220c9b15f69f11ce87958327e7
unknown <francoknotek27@gmail.com>
2022-10-05 13:55:33 -0300

    imprimiendo las 3 variables

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,8 @@ var Nombre1 = "Soy";
 let Nombre2 = 'El';
 const Nombre3 = `Barto`;
 // #endregion
+// #region Impresion1
+console.log(Nombre1);
+console.log(Nombre2);
+console.log(Nombre3);
+// #endregion
